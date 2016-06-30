33705ec459e0a4aa53a8d62eef7e3e6cba10816f
smilaxx <s.ky4.asu.0524@gmail.com>
2016-06-30 21:55:09 +0900

    taaaan

--- ッターン!/ッターン!/ViewController.swift
+++ ッターン!/ッターン!/ViewController.swift
@@ -13,40 +13,66 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
    
     var audioPlayer:AVAudioPlayer!
     
+    
     @IBOutlet var button:UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 再生するaudioファイルパスを取得
-        let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("kei_voice_008_1", ofType: "mp3")!)
+        let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("type", ofType: "mp3")!)
+        
         
         // auido を再生するプレイヤーを作成する
-        var audioError:NSError?
+        let audioError:NSError?=nil
         audioPlayer = try! AVAudioPlayer(contentsOfURL: audioPath)
         
         // エラーが起きたとき
         if let error = audioError {
             print("Error \(error.localizedDescription)")
         }
+        
+        
+        
+
         audioPlayer.delegate = self
         audioPlayer.prepareToPlay()
         
+        
+        try!AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+        try!AVAudioSession.sharedInstance().setActive(true)
+        audioPlayer.play()
         // Do any additional setup after loading the view, typically from a nib.
         
 
     }
 
+    
+  
     // ボタンがタップされた時の処理
     @IBAction func buttonTapped(sender : AnyObject) {
-        if ( audioPlayer.playing ){
-            audioPlayer.stop()
-            button.setTitle("Stop", forState: UIControlState.Normal)
-        }
-        else{
-            audioPlayer.play()
-            button.setTitle("Play", forState: UIControlState.Normal)
-        }
+//        let audioPlayerTaan:AVAudioPlayer!
+        
+        let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("taaan", ofType: "mp3")!)
+        
+         audioPlayer = try! AVAudioPlayer(contentsOfURL: audioPath)
+
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+        
+        
+//        if ( audioPlayer.playing ){
+//            audioPlayer.stop()
+//            
+//            button.setTitle("Stop", forState: UIControlState.Normal)
+//        }
+//        else{
+//            try!AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+//            try!AVAudioSession.sharedInstance().setActive(true)
+//            audioPlayerTaan.play()
+//            button.setTitle("Play", forState: UIControlState.Normal)
+//        }
     }
     
     override func didReceiveMemoryWarning() {
